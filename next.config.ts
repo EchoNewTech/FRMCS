476d2448f225c9ec829f5e8b6ebbfea5a25a73d3
Blat3 <154532277+Blat3@users.noreply.github.com>
2026-04-09 18:22:07 +0200

    część zmian w stronce z raspberki

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  allowedDevOrigins: ['192.168.0.87'],
+}
+
+
+export default nextConfig;
